7617ffca6470d3e3b9631f64524cb3efd2768dd1
vicdant1 <dantjv@outlook.com>
2023-08-26 03:16:00 -0300

    working on project

--- RunGroopWebApp/RunGroopWebApp/Controllers/ClubController.cs
+++ RunGroopWebApp/RunGroopWebApp/Controllers/ClubController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
 
 namespace RunGroopWebApp.Controllers
 {
     public class ClubController : Controller
     {
+        private AppDbContext _context { get; }
+
+        public ClubController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.Include(c => c.Address).ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var club = _context.Clubs.Include(c => c.Address).FirstOrDefault(x => x.Id == id);
+
+            return View(club);
         }
     }
 }
--- RunGroopWebApp/RunGroopWebApp/Controllers/RaceController.cs
+++ RunGroopWebApp/RunGroopWebApp/Controllers/RaceController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RunGroopWebApp.Data;
 
 namespace RunGroopWebApp.Controllers
 {
     public class RaceController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public RaceController(AppDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var races = _context.Races.Include(r => r.Address).ToList();
+            return View(races);
+        }
+
+
+        public IActionResult Detail(int id)
+        {
+            var race = _context.Races.Include(r => r.Address).FirstOrDefault(r => r.Id == id);
+            return View(race);
         }
     }
 }
